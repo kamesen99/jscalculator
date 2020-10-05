def29e6353047d2730e8e39bb1fda4747726357e
Miguel Pascual <miguel@yonder.com>
2020-10-04 23:43:34 -0700

    Updates

---
+++ precache-manifest.7ac73eef714906bf8165a1ff3064a60b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034c48384765188492566c82715cabbc",
+    "url": "/jscalculator/index.html"
+  },
+  {
+    "revision": "56241d65364d94a89a2a",
+    "url": "/jscalculator/static/css/main.aab9776c.chunk.css"
+  },
+  {
+    "revision": "717e8df122a59bb84285",
+    "url": "/jscalculator/static/js/2.8c95f80f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jscalculator/static/js/2.8c95f80f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56241d65364d94a89a2a",
+    "url": "/jscalculator/static/js/main.1d139ae6.chunk.js"
+  },
+  {
+    "revision": "45eac0e82cbb66ca6eb5",
+    "url": "/jscalculator/static/js/runtime-main.2d1ff128.js"
+  }
+]);
